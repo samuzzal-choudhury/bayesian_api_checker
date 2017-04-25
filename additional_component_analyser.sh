82c1355c101b2c915656b6c48c8fa3cc3692c4f4
Samuzzal Choudhury <samuzzal@redhat.com>
2017-04-25 15:36:59 +0530

    Additional tests, test pom.xml and new reports added

---
+++ additional_component_analyser.sh
@@ -0,0 +1,5 @@
+curl https://recommender.api.openshift.io/api/v1/component-analyses/maven/commons-fileupload:commons-fileupload/1.3
+sleep 3
+curl https://recommender.api.openshift.io/api/v1/component-analyses/maven/ch.qos.logback:logback-core/1.1.10
+sleep 3
+curl https://recommender.api.openshift.io/api/v1/component-analyses/npm/serve-static/1.7.1
---
+++ poms/readme.txt
@@ -0,0 +1,2 @@
+https://raw.githubusercontent.com/pmuir/vertx-http-booster/master/pom.xml
+https://github.com/qodfathr/trade-insights-service/blob/master/pom.xml
